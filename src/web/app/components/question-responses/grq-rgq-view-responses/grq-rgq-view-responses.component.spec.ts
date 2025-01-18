1004d66d2067fd9463e6cdf52eecb3c81f057288
Jose Eduardo <jevrprado@gmail.com>
2025-01-17 21:28:13 -0300

    add test for grq-view-responses
    Signed-off-by: Jose Eduardo <jevrprado@gmail.com>

--- src/web/app/components/question-responses/grq-rgq-view-responses/grq-rgq-view-responses.component.spec.ts
+++ src/web/app/components/question-responses/grq-rgq-view-responses/grq-rgq-view-responses.component.spec.ts
@@ -37,4 +37,57 @@ describe('GrqRgqViewResponsesComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //ct1
+  it('CT1: should display response when indicateMissingResponses is true and response is not missing', () => {
+    component.indicateMissingResponses = true;
+    component.responses = [
+      {
+        allResponses: [{ isMissingResponse: false, giver: 'User1', recipient: 'User2' }],
+      },
+    ] as any;
+  
+    component.ngOnInit(); 
+    expect(Object.keys(component.responsesToShow).length).toBeGreaterThan(0);
+  });
+
+  //ct2
+  it('CT2: should not display response when indicateMissingResponses is false and response is missing', () => {
+    component.indicateMissingResponses = false;
+    component.responses = [
+      {
+        allResponses: [{ isMissingResponse: true }],
+      },
+    ] as any;
+  
+    component.ngOnInit(); 
+    expect(Object.keys(component.responsesToShow).length).toBe(0);
+  });
+
+  //ct3
+  it('CT3: should not display response when sectionOfView does not match recipientSection', () => {
+    component.sectionOfView = 'SectionA';
+    component.responses = [
+      {
+        allResponses: [{ giverSection: 'SectionA', recipientSection: 'SectionB', giver: 'User1', recipient: 'User2' }],
+      },
+    ] as any;
+  
+    component.ngOnInit(); 
+    expect(Object.keys(component.responsesToShow).length).toBe(0);
+  });
+  
+  //ct4
+  it('CT4: should not display response when isGrq is true and giverSection does not match sectionOfView', () => {
+    component.isGrq = true;
+    component.sectionOfView = 'SectionB';
+    component.responses = [
+      {
+        allResponses: [{ giverSection: 'SectionA', recipientSection: 'SectionB', giver: 'User1', recipient: 'User2' }],
+      },
+    ] as any;
+  
+    component.ngOnInit(); 
+    expect(Object.keys(component.responsesToShow).length).toBe(0);
+  });
 });
